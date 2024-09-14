108e729d3e43651cc20cf21cb03542bf6168f355
Seyed Shayan Sajjadinia <40625615+shayansss@users.noreply.github.com>
2024-09-15 00:50:51 +0200

    Update Visualization.py
    Improve readability

--- Visualization.py
+++ Visualization.py
@@ -1,30 +1,32 @@
 from abaqus import *
 from abaqusConstants import *
-import section
-import regionToolset
-import displayGroupMdbToolset as dgm
-import part
-import material
-import assembly
-import step
-import interaction
-import load
-import mesh
-import optimization
-import job
-import sketch
-import visualization
-import xyPlot
 import displayGroupOdbToolset as dgo
-import connectorBehavior
-import time
 import os
-from shutil import copyfile
 import numpy as np
-from time import time
+import time
 
-class main_tools(object):
-    def __init__(self, jobName, partName, instanceName, modelName, mdb, session, viewportObj1 = 'Viewport: 1'):
+
+class MainTools(object):
+    """
+    A utility class for managing and interacting with ABAQUS output database (ODB) files and viewports.
+
+    This class provides methods to:
+    - Open an ODB file with error handling and retry mechanism.
+    - Save and close an ODB file.
+    - Set up and control viewport settings for displaying ODB data.
+
+    Attributes:
+        jobName (str): The name of the job associated with the ODB file.
+        odbName (str): The name of the ODB file, derived from jobName.
+        partName (str): The name of the part to be analyzed.
+        modelName (str): The name of the model in the ABAQUS database.
+        instanceName (str): The name of the instance within the model.
+        mdb: The ABAQUS model database object.
+        session: The ABAQUS session object.
+        viewportObj1: The viewport object used for displaying ODB data.
+    """
+
+    def __init__(self, jobName, partName, instanceName, modelName, mdb, session, viewportObj1='Viewport: 1'):
         self.jobName = jobName
         self.odbName = jobName + '.odb'
         self.partName = partName
@@ -33,137 +35,88 @@ class main_tools(object):
         self.mdb = mdb
         self.session = session
         self.viewportObj1 = self.session.viewports[viewportObj1]
-    
-    def job_submit(self, jobName):
-        FirstJob = self.mdb.jobs[jobName]
-        FirstJob.submit(consistencyChecking=OFF)
-        FirstJob.waitForCompletion()
-    
-    def open_odb(self, odbName, readOnly = True):
+
+    def open_odb(self, odbName, readOnly=True):
+        """
+        Open an ODB file with a retry mechanism in case of failure.
+        """
         try:
-            return self.session.openOdb(name = odbName, readOnly = readOnly)
-        except:
-            import time; time.sleep(5)
-            print 'open_odb() did not work.'
-            return self.session.openOdb(name = odbName, readOnly = readOnly)
-    
-    def close_odb(self, odbName = ''):
-        if odbName == '': odbName = self.odbName
-        try:
-            self.session.odbs[odbName].close()
-        except:
-            print 'close_odb did not work.'
-    
+            return self.session.openOdb(name=odbName, readOnly=readOnly)
+        except Exception as e:
+            time.sleep(5)
+            print(f'open_odb() did not work: {e}')
+            return self.session.openOdb(name=odbName, readOnly=readOnly)
+
     def save_and_close_odb(self, odbObj):
+        """
+        Save and close the ODB file.
+        """
         odbObj.save()
         odbObj.close()
-        
-    def output_values(self, odb, stepName = 'Main', frameNum = -1, parameterName = 'SDV3'):
-        return odb.steps[stepName].frames[frameNum].fieldOutputs[parameterName].values
-    
-    def edit_node_by_offset(self ,offsetPars):
-        self.mdb.meshEditOptions.setValues(enableUndo=True, maxUndoCacheElements=0.5)
-        partObj = self.mdb.models[self.modelName].parts[self.partName]
-        nodeObj = partObj.nodes
-        num = 0
-        for i in offsetPars:
-            num += 1
-            if num % 3 == 1:
-                nodes = i - 1
-            elif num % 3 == 2:
-                u1 = i
-            elif num % 3 == 0:
-                u2 = i
-                partObj.editNode(nodes=nodeObj[nodes], offset1=-u1, offset2=-u2, projectToGeometry=OFF)
-    
-    def extract_coords_values(self, frameNum = -1, stepName = 'Main', odb = ''):
-        if odb == '': odb = self.open_odb(self.odbName)
-        temp = []
-        dispVal = self.output_values(odb, stepName, frameNum, 'COORD')
-        for s in dispVal:
-            temp.append(s.nodeLabel)
-            temp.append(s.dataDouble[0])
-            temp.append(s.dataDouble[1])
-        
-        return temp
-        
-    def extract_nodal_values(self, frameNum = -1, stepName = 'Main', odb = ''):
-        if odb == '': odb = self.open_odb(self.odbName)
-        temp = []
-        dispVal = self.output_values(odb, stepName, frameNum, 'COORD')
-        for s in dispVal:
-            temp.append(s.nodeLabel)
-            temp.append(s.dataDouble[0])
-            temp.append(s.dataDouble[1])
-        
-        return temp
-    
-    def integration_points_values(self, parameters = ['LE22'], frameNum = -1, stepName = 'UC', odb = ''):
-        if odb == '': odb = self.open_odb(self.odbName)
-        temp = []
-        for sdv in parameters:
-            temp.append([])
-            outputValues = self.output_values(odb, stepName, frameNum, sdv)
-            for outputVal in outputValues:
-                temp[-1].append([outputVal.data, outputVal.elementLabel, outputVal.integrationPoint])
-        
-        return temp
 
+
+# Change the current working directory to the specified path and open the .cae file
 os.chdir(r"C:\Temp\PointMSE")
 FileName = 'abq'
-openMdb(pathName = FileName + '.cae')
-# mdb.saveAs(pathName = FileName + '-Backup.cae')
+openMdb(pathName=FileName + '.cae')
 openMdb(pathName=FileName + '.cae')
 
+# Initialize job, part, instance, and model names, and open an output database (ODB) file
 jobName, partName, instanceName, modelName = 'Job', 'part', 'part', 'Model-1'
 modelObj = mdb.models[modelName]
-mt = main_tools(jobName, partName, instanceName, modelName, mdb, session) # main tools of Abaqus
-odb = mt.open_odb(mt.odbName, readOnly = False)
+mt = MainTools(jobName, partName, instanceName, modelName, mdb, session)
+odb = mt.open_odb(mt.odbName, readOnly=False)
 
+# Configure the viewport settings for displaying the ODB and disable annotation options
 viewportObj = session.viewports['Viewport: 1']
 viewportObj.setValues(displayedObject=odb)
 viewportObj.makeCurrent()
-
-
-viewportObj.viewportAnnotationOptions.setValues(triad=OFF, legend=OFF, title=OFF, state=OFF, annotations=OFF, compass=OFF)
+viewportObj.viewportAnnotationOptions.setValues(
+    triad=OFF, legend=OFF, title=OFF, state=OFF, annotations=OFF, compass=OFF
+)
 
 # START OF AUTOMATICALLY GENERATED PYTHON CODE 1
 path = "temp_visualization"
 # END OF AUTOMATICALLY GENERATED PYTHON CODE 1
-print path
-data = np.loadtxt(path, delimiter=",", dtype='float32')
-data = data.reshape(-1,1)
 
+# Load data from the specified CSV file and reshape it
+data = np.loadtxt(path, delimiter=",", dtype='float32')
+data = data.reshape(-1, 1)
+
+# Retrieve stress field data from the ODB for a specific region at the element nodal position
 odbInstance = odb.rootAssembly.instances['PART']
-Sarrays   = odb.steps['static'].frames[-1].fieldOutputs['S'].getSubset(region = odbInstance, position = ELEMENT_NODAL).values
+Sarrays = odb.steps['static'].frames[-1].fieldOutputs['S'].getSubset(
+    region=odbInstance, position=ELEMENT_NODAL).values
 region = [i.nodeLabel for i in Sarrays]
 
-name = 'temp_%s'%(np.random.random())
-
-
-newFieldOutput = odb.steps['static'].frames[0].FieldOutput(name = name, description = '', type = SCALAR)
-newFieldOutput.addData(position=NODAL, instance = odbInstance, labels = region, data = data)
+# Generate a random field output name and add the reshaped data as a new field output to the ODB
+name = 'temp_%s' % (np.random.random())
+newFieldOutput = odb.steps['static'].frames[0].FieldOutput(
+    name=name, description='', type=SCALAR)
+newFieldOutput.addData(
+    position=NODAL, instance=odbInstance, labels=region, data=data)
 mt.save_and_close_odb(odb)
-odb = mt.open_odb(mt.odbName, readOnly = False)
+odb = mt.open_odb(mt.odbName, readOnly=False)
 
+# Set the viewport to display the newly updated ODB and select the first step and frame
 viewportObj = session.viewports['Viewport: 1']
 viewportObj.setValues(displayedObject=odb)
 viewportObj.makeCurrent()
-
 viewportObj.odbDisplay.setFrame(step=0, frame=0)
 
-viewportObj.odbDisplay.display.setValues(plotState=(CONTOURS_ON_DEF, ))
-leaf = dgo.LeafFromElementSets(elementSets=(' ALL ELEMENTS', ))
+# Set the contour plot display settings in the viewport
+viewportObj.odbDisplay.display.setValues(plotState=(CONTOURS_ON_DEF,))
+leaf = dgo.LeafFromElementSets(elementSets=(' ALL ELEMENTS',))
 viewportObj.odbDisplay.displayGroup.replace(leaf=leaf)
+
 # START OF AUTOMATICALLY GENERATED PYTHON CODE 2
 viewportObj.odbDisplay.contourOptions.setValues(maxAutoCompute=OFF, maxValue=4, minAutoCompute=OFF, minValue=0)
 # END OF AUTOMATICALLY GENERATED PYTHON CODE 2
+
+# Fit the view to the viewport, set contour display settings, and save the current viewport as an image
 viewportObj.view.fitView()
 viewportObj.odbDisplay.contourOptions.setValues(outsideLimitsMode=SPECTRUM)
-
 viewportObj.odbDisplay.setPrimaryVariable(variableLabel=name, outputPosition=NODAL)
 session.pngOptions.setValues(imageSize=(4000, 2536))
 session.printOptions.setValues(reduceColors=False)
-session.printToFile(fileName='%s'%(path), format=PNG, canvasObjects=(viewportObj, ))
-
-
+session.printToFile(fileName='%s' % (path), format=PNG, canvasObjects=(viewportObj,))
